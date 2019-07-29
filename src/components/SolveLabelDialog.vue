1d01af1ce34f35714d03dd1dcc5bd13368dd39e2
Dominik Korsa <dominik.korsa@gmail.com>
2019-07-29 19:54:45 +0200

    Add solve labels editing

--- src/components/SolveLabelDialog.vue
+++ src/components/SolveLabelDialog.vue
@@ -1,66 +1,96 @@
 <template>
-  <v-dialog
-    v-model="dialogActive"
-    width="400"
-  >
-    <v-card>
-      <v-toolbar color="primary white--text">
-        <v-toolbar-title>Solve label</v-toolbar-title>
-      </v-toolbar>
+  <div>
+    <v-dialog
+      v-model="dialogActive"
+      width="400"
+      :persistent="snackbar"
+    >
+      <v-card>
+        <v-toolbar color="primary darken-1 white--text">
+          <v-toolbar-title>Solve label</v-toolbar-title>
+          <v-spacer></v-spacer>
+          <v-scale-transition>
+            <v-btn v-if="label !== null && renamedLabel === null" icon @click="startRenaming" dark>
+              <v-icon>mdi-rename-box</v-icon>
+            </v-btn>
+          </v-scale-transition>
+          <v-scale-transition>
+            <v-btn v-if="label !== null && renamedLabel === null" icon @click="remove" dark>
+              <v-icon>mdi-delete</v-icon>
+            </v-btn>
+          </v-scale-transition>
+        </v-toolbar>
 
-      <v-list class="pb-0">
-        <v-list-item-group
-          :value="label || 'normal'"
-          @change="selectedLabelChange"
-          mandatory color="primary">
-          <solve-label-dialog-item value="normal">
-            Normal
-          </solve-label-dialog-item>
-          <solve-label-dialog-item :value="label.id" v-for="label in labels" :key="label.id">
-            {{ label.name }}
-          </solve-label-dialog-item>
-        </v-list-item-group>
+        <v-list class="pb-0">
+          <v-list-item-group
+            :value="label || 'normal'"
+            @change="selectedLabelChange"
+            mandatory color="primary">
+            <solve-label-dialog-item value="normal">Normal</solve-label-dialog-item>
+            <solve-label-dialog-item
+              :value="label.id" v-for="label in labels"
+              :key="label.id"
+              :renaming="renamedLabel === label.id"
+              @rename="rename">
+              {{ label.name }}
+            </solve-label-dialog-item>
+          </v-list-item-group>
+
+          <v-divider></v-divider>
+
+          <v-form @submit.prevent="addLabelSubmit">
+            <v-list-item
+              color="secondary"
+              class="add-label v-list-item--active"
+              @click="addLabelClick"
+              :ripple="false"
+              inactive
+              @keydown.stop
+              @keyup.stop>
+              <v-list-item-action>
+                <v-icon>mdi-plus</v-icon>
+              </v-list-item-action>
+
+              <v-list-item-title class="black--text">
+                <input class="add-label__input" placeholder="New label" v-model="newLabel">
+              </v-list-item-title>
+
+              <v-list-item-action class="add-label__button-action" v-if="newLabel.length > 0">
+                <v-btn type="submit" text color="secondary" @click.stop>Add</v-btn>
+              </v-list-item-action>
+            </v-list-item>
+          </v-form>
+        </v-list>
 
         <v-divider></v-divider>
 
-        <v-form @submit.prevent="addLabelSubmit">
-          <v-list-item
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
             color="secondary"
-            class="add-label v-list-item--active"
-            @click="addLabelClick"
-            :ripple="false"
-            inactive
-            @keydown.stop
-            @keyup.stop>
-            <v-list-item-action>
-              <v-icon>mdi-plus</v-icon>
-            </v-list-item-action>
-
-            <v-list-item-title class="black--text">
-              <input class="add-label__input" placeholder="New label" v-model="newLabel">
-            </v-list-item-title>
-
-            <v-list-item-action class="add-label__button-action" v-if="newLabel.length > 0">
-              <v-btn type="submit" text color="secondary" @click.stop>Add</v-btn>
-            </v-list-item-action>
-          </v-list-item>
-        </v-form>
-      </v-list>
-
-      <v-divider></v-divider>
-
-      <v-card-actions>
-        <v-spacer></v-spacer>
-        <v-btn
-          color="secondary"
-          text
-          @click="dialogActive = false"
-        >
-          Close
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-  </v-dialog>
+            text
+            @click="dialogActive = false"
+          >
+            Close
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+    <v-snackbar
+      v-model="snackbar"
+      :timeout="5000"
+      color="error"
+    >
+      You can't remove a label if there are solves assigned to it
+      <v-btn
+        dark
+        text
+        @click="snackbar = false"
+      >
+        Close
+      </v-btn>
+    </v-snackbar>
+  </div>
 </template>
 
 <script>
@@ -76,11 +106,50 @@ export default {
     return {
       dialogActive: this.value,
       newLabel: '',
+      renamedLabel: 'XmWIUCbkeQIj8M71cdXS',
+      snackbar: false,
     };
   },
   methods: {
     addLabelClick() {
       document.getElementsByClassName('add-label__input')[0].focus();
+    },
+    rename(value) {
+      const { currentUser } = firebase.auth();
+
+      if (!currentUser) return;
+
+      const label = firebase.firestore().collection('users')
+        .doc(currentUser.uid).collection('solve-labels')
+        .doc(this.renamedLabel);
+      label.update({ name: value });
+      this.renamedLabel = null;
+    },
+    async remove() {
+      const { currentUser } = firebase.auth();
+
+      if (!currentUser) return;
+
+      const solves = await firebase.firestore().collection('users')
+        .doc(currentUser.uid).collection('solves')
+        .where('label', '==', this.label)
+        .get();
+
+      if (solves.size > 0) {
+        this.snackbar = true;
+        return;
+      }
+
+      const label = firebase.firestore().collection('users')
+        .doc(currentUser.uid).collection('solve-labels')
+        .doc(this.label);
+
+      this.selectedLabelChange('normal');
+
+      label.delete();
+    },
+    startRenaming() {
+      this.renamedLabel = this.label;
     },
     async addLabelSubmit() {
       if (this.newLabel.length === 0) return;
@@ -102,6 +171,10 @@ export default {
       this.newLabel = '';
     },
     selectedLabelChange(value) {
+      if (value !== undefined && value !== this.label) {
+        this.renamedLabel = null;
+      }
+
       if (value === 'normal') {
         this.$emit('update:label', null);
       } else {
@@ -115,6 +188,9 @@ export default {
     },
     value(value) {
       this.dialogActive = value;
+      if (!value) {
+        this.renamedLabel = null;
+      }
     },
   },
   props: {
--- src/components/SolveLabelDialogItem.vue
+++ src/components/SolveLabelDialogItem.vue
@@ -1,5 +1,27 @@
 <template>
+  <v-form @submit.prevent="renameSubmit" v-if="renaming">
+    <v-list-item
+      color="secondary"
+      class="rename"
+      :value="value"
+      :ripple="false"
+      @keydown.stop
+      @keyup.stop>
+      <v-list-item-action>
+        <v-icon>mdi-rename-box</v-icon>
+      </v-list-item-action>
+
+      <v-list-item-title class="black--text">
+        <input class="rename__input" placeholder="New label" v-model="newName">
+      </v-list-item-title>
+
+      <v-list-item-action class="rename__button-action" v-if="newName.length > 0">
+        <v-btn type="submit" text color="secondary" @click.stop>Rename</v-btn>
+      </v-list-item-action>
+    </v-list-item>
+  </v-form>
   <v-list-item
+    v-else
     :value="value"
     v-slot:default="{ active, toggle }">
     <template>
@@ -12,19 +34,59 @@
     <v-list-item-title>
       <slot></slot>
     </v-list-item-title>
-
-    <v-list-item-action>
-
-    </v-list-item-action>
   </v-list-item>
 </template>
 
 <script>
 export default {
   props: {
+    renaming: {
+      default: false,
+      type: Boolean,
+    },
     value: {
       type: String,
     },
   },
+  data: () => ({
+    newName: '',
+  }),
+  methods: {
+    renameSubmit() {
+      if (!this.newName) return;
+      this.$emit('rename', this.newName);
+      this.newName = '';
+    },
+  },
+  watch: {
+    renaming: {
+      immediate: true,
+      handler(value) {
+        if (value) {
+          this.newName = this.$slots.default[0].text.trim();
+        }
+      },
+    },
+  },
 };
 </script>
+
+<style lang="scss" scoped>
+  .rename {
+    cursor: pointer;
+  }
+
+  .rename__button-action {
+    min-width: auto;
+    margin: 0px;
+    margin-left: 16px;
+  }
+
+  .rename__input {
+    outline: none;
+
+    &:focus::placeholder {
+      color: transparent;
+    }
+  }
+</style>

d24471b7b139dfafe978ecb564bcd35666b3bbea
Dominik Korsa <dominik.korsa@gmail.com>
2019-08-21 12:27:54 +0200

    Fix Vuetify Loader

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
+const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin');
+
 module.exports = {
   devServer: {
     disableHostCheck: true,
+  },
+  configureWebpack: {
+    plugins: [
+      new VuetifyLoaderPlugin(),
+    ],
   },
 };
